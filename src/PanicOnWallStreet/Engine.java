abce1e97256dad6c7557c062c1f885adc829c20d
gribeirofeup <goncalo.ribeiro@fe.up.pt>
2017-10-24 15:15:33 +0100

    turned the class Engine into a singleton

--- src/PanicOnWallStreet/Engine.java
+++ src/PanicOnWallStreet/Engine.java
@@ -12,7 +12,14 @@ import java.util.Random;
 
 public class Engine {
 
-	public enum Phase{negotiation,investors,managers,payment,auction}
+	public enum Phase{
+		negotiation,
+		investors,
+		managers,
+		payment,
+		auction
+	};
+	
 	public enum Color{
 		red(0),yellow(1),green(2),blue(3);
 		private int i;
@@ -49,43 +56,71 @@ public class Engine {
 	private static Integer diceRoll(Color c){
 		int i = RNG.nextInt(6);
 		return DICE[c.index()][i];		
+	}		
+	
+	public static List<Player> players = new ArrayList<Player>();
+	private static Phase phase = Phase.negotiation;
+	private static Integer turn = 0;
+
+	public Engine(){}
+	public static void addPlayer(Player p){ players.add(p); }
+	public static void start(){
+		for(; turn < NR_TURNS; turn++)
+			startTurn();
+		reset();
 	}
+
+	private static void startTurn(){
+		switch(phase){
+			case negotiation:
+				startNegotiation();	// starts company negotiation protocol
+				break;
+			case investors:				
+				fluctuate();		// fluctuate the companies' values
+				investorsIncome();	// calculate the investors' income
+				break;
+			case managers:
+				managersIncome();	// calculate the managers' income
+				break;
+			case payment:
+				managersPayment();	// managers cost payment to the bank
+				break;
+			case auction:
+				startAuction();		// starts company auction protocol
+				break;
+		}
+	}
+	
+	private static void reset(){
+		turn = 0;
+		phase = Phase.negotiation;
+	}
+	
 	private static void fluctuate(){
 		for(Color c = Color.red; c.index() < Color.NR_COLORS; c.next()){
 			pointers[c.index()] += diceRoll(c);
 			pointers[c.index()] %= 8;
 		}
 	}
-
-	public static List<Player> players = new ArrayList<Player>();
-	private Phase phase = Phase.negotiation;
-	private Integer turn = 0;
-
-	public Engine(){}
-	public void addPlayer(Player p){}
-	public void start(){
-		for(; turn < NR_TURNS; turn++)
-			startTurn();
-	}
-
-	private void startTurn(){
-		switch(phase){
-			case negotiation:
-
-				break;
-			case investors:
-				fluctuate();
-				break;
-			case managers: break;
-			case payment: break;
-			case auction: break;
-		}
-	}
-
-	public void reset(){
-		turn = 0;
-		phase = Phase.negotiation;
+	
+	public static void investorsIncome(){
+		
 	}
 	
+	public static void managersIncome(){
+		
+	}
+	
+	public static void startAuction(){
+		
+	}
+	
+	public static void startNegotiation(){
+		
+	}
+	
+	public static void managersPayment(){
+		
+	}
 	
 }
